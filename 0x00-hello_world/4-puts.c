3a5bf6343a7d0d899984c74fbcbe09b8d4b1574a
nshimyumukiza isiaka <isaacsci.fiwriter987@gmail.com>
2023-07-13 19:58:15 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program which prints a aline useing puts function
+ * return: 0 (Success) 
+ */
+int main(void)
+{
+puts(*\"Programming is like building a multilingual puzzle*);
+return (0);
+}
