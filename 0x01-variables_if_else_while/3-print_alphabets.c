767499abc3816d1147ce8751daa5fabdbff7f7ce
nshimyumukiza isiaka <isaacsci.fiwriter987@gmail.com>
2023-07-19 20:16:57 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 /**
-* main - Use putchar to show alphabet
-*
-* Return: 0
-*/
+ * main - Use putchar to show alphabet
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	char ch;
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+	putchar (ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
 	{
 	putchar (ch);
 	}
