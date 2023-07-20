9ec349c0b31ba2b216096f33bf6488bd2a4a3358
nshimyumukiza isiaka <isaacsci.fiwriter987@gmail.com>
2023-07-19 22:27:08 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
-*main - prints out _putchar
-*
-*Return: none
-*/
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	_putchar('_');
@@ -15,5 +14,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
+
