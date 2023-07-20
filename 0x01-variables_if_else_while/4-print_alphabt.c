6f64fabe492690f108bce49cfb004a20df73a221
nshimyumukiza isiaka <isaacsci.fiwriter987@gmail.com>
2023-07-19 20:23:34 -0700

    when I was

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	putchar('\n');
+	return (0);
+}
